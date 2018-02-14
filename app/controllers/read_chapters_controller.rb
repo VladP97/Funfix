2f2951c4b2adad0bbff708d3114991be9b58c327
Unknown <vlad.potapov1997@gmail.com>
2018-02-14 14:46:10 +0300

    Added pagination to chapters

--- app/controllers/read_chapters_controller.rb
+++ app/controllers/read_chapters_controller.rb
@@ -2,12 +2,27 @@ class ReadChaptersController < ReadFanficsController
   def show
     @chapter = Chapter.find(params[:id])
     @fanfic = Fanfic.find(params[:read_fanfic_id])
-    @rating = Rating.average(:rating)
-    if Rating.where(user_id: current_user.id, chapter_id: params[:id]).to_a != []
-      @user_rating = Rating.where(user_id: current_user.id)[0][:rating]
-    else
-      @user_rating = "You don't rate this post yet"
-    end
+    @chapters = @fanfic.chapters
+    show_average_rating(Rating.where(chapter_id: params[:id]))
+    show_user_rating(Rating.where(user_id: current_user.id, chapter_id: params[:id]))
     @comment = Comment.new
+  end
+
+  private
+
+  def show_user_rating(rating)
+    if rating.any?
+      @user_rating = rating[0][:rating]
+    else
+      @user_rating = "You haven't rate this post yet"
+    end
+  end
+
+  def show_average_rating(ratings)
+    if ratings.any?
+      @rating = ratings.average(:rating)
+    else
+      @rating = "No one rate this post yet"
+    end
   end
 end
--- app/models/fanfic.rb
+++ app/models/fanfic.rb
@@ -1,4 +1,5 @@
 class Fanfic < ApplicationRecord
   has_many :comments
   has_many :users, through: :comments
+  has_many :chapters
 end
