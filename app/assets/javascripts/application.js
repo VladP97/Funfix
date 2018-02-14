2502b2d084314e6b011baf6d27b0dd5485e6eaba
Unknown <vlad.potapov1997@gmail.com>
2018-02-14 16:37:03 +0300

    Added ability to sort data in tables

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require popper
 //= require jquery
 //= require jquery_ujs
+//= require dataTables/jquery.dataTables
+//= require dataTables/extras/dataTables.responsive
 //= require bootstrap
 //= require bootstrap-sprockets
 //= require markitup
