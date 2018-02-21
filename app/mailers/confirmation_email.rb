32db26b19f300cfb2d2aacfde815bf06126d4048
Unknown <vlad.potapov1997@gmail.com>
2018-02-21 18:56:29 +0300

    SMTP was setupped

--- app/mailers/confirmation_email.rb
+++ app/mailers/confirmation_email.rb
@@ -1,4 +1,5 @@
 class ConfirmationEmail < Devise::Mailer
+  default from: "vlad.potapov@gmail.com"
   helper :application
   include Devise::Controllers::UrlHelpers
   default template_path: 'devise/mailer'
